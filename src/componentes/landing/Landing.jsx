4c7d8e95932f490caac91864cd0cc28c67bfc229
Facu Sanchez <facusanchz96@gmail.com>
2024-08-21 11:02:56 -0300

    landing style

--- src/componentes/landing/Landing.jsx
+++ src/componentes/landing/Landing.jsx
@@ -30,7 +30,7 @@ export default function Landing() {
       <div className={styles.feature}>
         <img src="https://static.vecteezy.com/system/resources/previews/014/932/882/non_2x/document-check-icon-outline-form-paper-vector.jpg" alt="Característica 1" />
         <div className={styles.text}>
-          <h3>Registrar muestras farmacéuticas fácilmente.</h3>
+          <h3>Registra muestras fácilmente.</h3>
           {/* <p>Registrar nuevas muestras farmacéuticas fácilmente..</p> */}
         </div>
       </div>
