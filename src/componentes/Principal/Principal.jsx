0296406d0eb8d92aeb3353d52dc50b0b570141a2
Facu Sanchez <facusanchz96@gmail.com>
2024-10-04 11:29:30 -0300

    menu

--- src/componentes/Principal/Principal.jsx
+++ src/componentes/Principal/Principal.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SearchBar from '../searchbar/SearchbBar';
 import Nav from '../nav/Nav';
-
+import Menu from '../menu/Menu'
 export default function Principal() {
   return (
     
          <div >
             <Nav></Nav>
         <SearchBar />
+        <Menu></Menu>
     
       <div >
       </div>
