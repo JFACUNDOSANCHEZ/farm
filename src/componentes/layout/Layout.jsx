57c467f77fc946d8e242156f15d19e20a7914931
Facu Sanchez <facusanchz96@gmail.com>
2024-10-28 11:07:50 -0300

    style

--- src/componentes/layout/Layout.jsx
+++ src/componentes/layout/Layout.jsx
@@ -1,19 +1,21 @@
-// componentes/Layout/Layout.jsx
 import React from 'react';
 import Menu from '../menu/Menu'; // Importas tu menú
 import Nav from '../nav/Nav';    // Y tu componente de navegación
 import styles from './Layout.module.css'; // Importa los estilos
 import Filtros from '../filtros/Filtros';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children }) => {
+  const location = useLocation(); // Mueve useLocation dentro del componente
+
   return (
     <div className={styles.container}>
       <div className={styles.mainContent}> {/* Contenedor para el contenido principal */}
-      <Menu className={styles.menu} />   {/* Aplicar clase al menú */}
-        <Nav />    Renderiza la barra de navegación
+        <Menu className={styles.menu} />   {/* Aplicar clase al menú */}
+        <Nav /> {/* Renderiza la barra de navegación */}
+        
         <div className={styles.filtro}>
-
-        <Filtros />
+          {location.pathname === '/ensayos' ? null : <Filtros />} {/* Condición para renderizar Filtros */}
         </div>
         
         <main className={styles.contenido}>
