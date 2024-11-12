ffe68e422dd070d50454deda54048a78d7623ad9
Facu Sanchez <facusanchz96@gmail.com>
2024-11-12 10:28:33 -0300

    matrices

--- src/componentes/matrices/Matrices.jsx
+++ src/componentes/matrices/Matrices.jsx
@@ -21,45 +21,56 @@ const Matrices = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <h2 className={styles.title}>Matrices</h2>
-        <button onClick={toggleViewMode} className={styles.toggleButton}>
-          Ver {viewMode === 'cards' ? 'Tabla' : 'Cards'}
-        </button>
-      </div>
-
-      {viewMode === 'cards' ? (
-        <div className={styles.cardContainer}>
-          {list.map((matriz, index) => (
-            <div key={index} className={styles.card}>
-              <p className={styles.cardDescription}><strong>{matriz.descripcion}</strong></p>
-              <p className={styles.cardCode}><strong>Código:</strong> {matriz.codigo.trim()}</p>
-              <p className={styles.cardSample}><strong>Muestra:</strong> {matriz.muestra.trim()}</p>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Descripción</th>
-              <th>Código</th>
-              <th>Muestra</th>
-            </tr>
-          </thead>
-          <tbody>
-            {list.map((matriz, index) => (
-              <tr key={index}>
-                <td>{matriz.descripcion}</td>
-                <td>{matriz.codigo.trim()}</td>
-                <td>{matriz.muestra.trim()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    <div className={styles.header}>
+   <h2 className={styles.title}>Matrices</h2>
+   <button onClick={toggleViewMode} className={styles.toggleButton}>
+     Ver  {viewMode === 'cards' ? 'Tabla' : 'Cards'}
+   </button>
+ </div>
+ 
+       {viewMode === 'cards' ? (
+         <div className={styles.cardContainer}>
+           {list.map((metodo, index) => (
+             <div key={index} className={styles.card}>
+               <img 
+                 src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" 
+                 className={styles.cardImage} 
+                 alt={`Avatar de ${metodo.descripcion}`} 
+               />
+               <h3 className={styles.cardTitle}>{metodo.descripcion}</h3>
+               <p className={styles.cardCode}>Código: {metodo.codigo.trim()}</p>
+               <p className={styles.cardCode}>Nivel: {metodo.nivel}</p>
+             </div>
+           ))}
+         </div>
+       ) : (
+         <table className={styles.table}>
+           <thead>
+             <tr>
+               
+               <th>Nombre</th>
+               <th>Código</th>
+               <th>Nivel</th>
+             </tr>
+           </thead>
+           <tbody>
+             {list.map((metodo, index) => (
+               <tr key={index}>
+                
+                 <td>{metodo.descripcion}</td>
+                 <td>{metodo.codigo.trim()}</td>
+                 <td>{metodo.muestra.trim()}</td>
+               </tr>
+             ))}
+           </tbody>
+         </table>
+       )}
+     </div>
   );
 };
 
 export default Matrices;
+
+{/* <p className={styles.cardDescription}><strong>{matriz.descripcion}</strong></p>
+<p className={styles.cardCode}><strong>Código:</strong> {matriz.codigo.trim()}</p>
+<p className={styles.cardSample}><strong>Muestra:</strong> {matriz.muestra.trim()}</p> */}
