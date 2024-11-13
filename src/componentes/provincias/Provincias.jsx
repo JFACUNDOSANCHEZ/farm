7be650af85a0d8fdfe779142d1e12b5c6bd7d0f4
Facu Sanchez <facusanchz96@gmail.com>
2024-11-13 10:45:06 -0300

    stilo

--- src/componentes/provincias/Provincias.jsx
+++ src/componentes/provincias/Provincias.jsx
@@ -5,10 +5,7 @@ import styles from './provincias.module.css';
 import { useEffect, useState } from 'react';
 
 const iconosProvincias = {
-  "A": "https://img.freepik.com/vector-premium/mapa-silueta-salta-argentina_721965-3448.jpg", // Icono de Salta
-  "B": "https://static.thenounproject.com/png/172580-200.png", // Icono de Buenos Aires
-  "C": "https://static.thenounproject.com/png/172601-200.png", // Icono de Capital Federal
-  "E": "https://media.istockphoto.com/id/1158098054/es/vector/%C3%A1-%C3%A1-1-2%C3%B1%C3%A11-2-%C3%B1%C3%A1.jpg?s=612x612&w=0&k=20&c=XiGYsximS967RXee2xIPXl4tlNxdpUbUJ8X7yK-2IPY=", // Icono de Entre Ríos
+ 
   // Añadir el resto de provincias aquí...
   "EXTER": "https://link_a_icono_exterior.png" // Icono de Exterior del país
 };
@@ -44,7 +41,7 @@ const Provincias = () => {
                     {provincias.map((metodo, index) => (
                         <div key={index} className={styles.card}>
                             <img 
-                                src={iconosProvincias[metodo.codigo.trim()] || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"} 
+                                src={iconosProvincias[metodo.codigo.trim()] || "https://static.thenounproject.com/png/674232-200.png"} 
                                 className={styles.cardImage} 
                                 alt={`Icono de ${metodo.descripcion}`} 
                             />
